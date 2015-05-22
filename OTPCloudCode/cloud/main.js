18c8a98af3f4a1b7a3bbe086c0ba148314f30777
Bill Lin <kookehmonsters@yahoo.com>
2015-05-22 00:05:22 -0700

    Added location functionality

--- OTPCloudCode/cloud/main.js
+++ OTPCloudCode/cloud/main.js
@@ -1,6 +1,25 @@
+Parse.Cloud.define("getNear", function(request, response) {
+    var userQuery = new Parse.Query(Parse.User);
+    userQuery.equalTo("username", request.params.username);
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+    userQuery.first({
+        success: function(user) {
+            var locationQuery = new Parse.Query(Parse.User);
+            locationQuery.limit(10);
+            locationQuery.notEqualTo("username", request.params.username);
+            locationQuery.withinMiles("location", user.get("location"), 50);
+
+            locationQuery.find({
+                success: function(results) {
+                    response.success(results)
+                },
+                error: function(error) {
+                    response.error("Found no user(s) within range");
+                }
+            });
+        },
+        error: function(error) {
+            response.error("Unable to retrieve nearby user(s)");
+        }
+    });
 });
